1b63a57b999352ede46b48c2dc483bcfac8f264a
vpdesai21 <vishwa.desai@sjsu.edu>
2015-12-03 13:22:41 -0800

    Updated for google analytics and localization

---
+++ node_app/public/js/custom.js
@@ -0,0 +1,23 @@
+$(function() {
+
+	checkLoggedIn();
+});
+
+
+function checkLoggedIn() {
+	if(localStorage.loggedin == 'josh') {
+		$('#logindiv').hide(); 
+    	$('#profilediv').show();
+	}
+	
+}
+
+function login(){
+    if(typeof(Storage) !== "undefined") {
+        localStorage.setItem("loggedin", "josh");
+    } else {
+        // Sorry! No Web Storage support..
+    }
+    $('#logindiv').hide(); 
+    $('#profilediv').show();
+}
